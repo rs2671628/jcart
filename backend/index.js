ed41effa818ecac48921e4c8c8f25bac8c48cc07
Rohit Singha <87355154+rs2671628@users.noreply.github.com>
2024-06-06 15:55:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const app=express()
 app.use(cors({
-    origin: ["https://deploy-mern-frontend.vercel.app"],
+    origin: ["https://https://jcart-ebfb.vercel.app/"],
     methods: ["POST", "GET"],
     credentials: true
 }))
