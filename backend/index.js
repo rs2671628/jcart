6f2931953cea2235e0f852693e0e5edc5f200a0d
Rohit Singha <87355154+rs2671628@users.noreply.github.com>
2024-06-18 20:49:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ require('dotenv').config()
 const app=express()
 app.use(cors({
     origin:process.env.FRONTEND_URL,
+     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials:true,
 }))
 app.use(express.json())
